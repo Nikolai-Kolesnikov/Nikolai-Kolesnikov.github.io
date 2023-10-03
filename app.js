04ea77655837eed815c817139715d391651a8b68
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2023-10-03 22:11:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-document.getElementById("verh").innerHTML = "dynamic 6";
+document.getElementById("verh").innerHTML = "dynamic 7";
 let tg = window.Telegram.WebApp;
 
-let strtrtr = `dynamic 6 ### ${tg.initDataUnsafe.user.id} ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+let strtrtr = `dynamic 7 ### <br>initData = ${tg.initData}<br>${tg.initDataUnsafe.user.id} ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 document.getElementById("verh").innerHTML = strtrtr;
