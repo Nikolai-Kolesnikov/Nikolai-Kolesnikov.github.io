c8a04fcfd945549cb2e937ed38c4f24dd221d233
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2023-10-03 21:34:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let tg = window.Telegram.WebApp;
 
 let p = document.createElement("p");
 
-p.innerText = `Ты кто 2`;
+p.innerText = `Ты кто 3`;
 let strtrtr = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
