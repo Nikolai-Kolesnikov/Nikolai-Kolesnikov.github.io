5bd1fac5811e646cea8b6aa425ca0c21cfa8cbfe
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2023-10-03 21:33:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,11 @@
 document.getElementById("verh").innerHTML = "Прива 2";
 let tg = window.Telegram.WebApp;
 
-
-
-let usercard = document.getElementById("usercard");
-
 let p = document.createElement("p");
 
 p.innerText = `Ты кто 2`;
 let strtrtr = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
+document.body.appendChild(p);
 
