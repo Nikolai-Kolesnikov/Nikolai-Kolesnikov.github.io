84dc1a6cfbe0f6c34db59e5b838de5999206d836
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2023-10-04 16:25:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
-document.getElementById("verh").innerHTML = "dynamic 9";
+document.getElementById("verh").innerHTML = "dynamic 10";
+
+function isFetchAPISupported() {
+    return 'fetch' in window;
+}
+
 let tg = window.Telegram.WebApp;
 
-let strtrtr = `dynamic 9 ### <br>initData = ${tg.initData}<br>${tg.initDataUnsafe.user.id} ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+//let strtrtr = `dynamic 10 ### <br>initData = ${tg.initData}<br>${tg.initDataUnsafe.user.id} ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+let strtrtr = `isFetchAPISupported() = ${isFetchAPISupported()}`;
 document.getElementById("verh").innerHTML = strtrtr;
