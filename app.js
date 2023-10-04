ab9f2486358178fbca7c8899e13524f089ddbd33
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2023-10-04 17:03:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ function isFetchAPISupported() {
     return 'fetch' in window;
 }
 
+/*fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: 'post',
+    body: post,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}).then((response) => {
+    return response.json()
+}).then((res) => {
+    if (res.status === 201) {
+        console.log("Post successfully created!")
+    }
+}).catch((error) => {
+    console.log(error)
+});*/
+
 let tg = window.Telegram.WebApp;
 
 //let strtrtr = `dynamic 10 ### <br>initData = ${tg.initData}<br>${tg.initDataUnsafe.user.id} ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
