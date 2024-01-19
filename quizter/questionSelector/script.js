59702600865e70cb2d407b8ed8012757071d41f4
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2024-01-20 00:55:07 +0300

    Update script.js

--- quizter/questionSelector/script.js
+++ quizter/questionSelector/script.js
@@ -1,6 +1,6 @@
 // script.js 
 
-import {webappRequest} from '../webappRequest.js'; // функция для отправки ajax-запросов
+import {webappRequest} from '/webappRequest.js'; // функция для отправки ajax-запросов
 
 const table = document.getElementById("table"); 
 
