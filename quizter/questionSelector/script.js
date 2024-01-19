90174ff09ca24b0e70a15efc44f5d6b7a522355a
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2024-01-19 13:06:31 +0300

    Update script.js

--- quizter/questionSelector/script.js
+++ quizter/questionSelector/script.js
@@ -25,11 +25,9 @@ function addItem(e, i) {
 	c1.innerText = e.modifiedAt; 
 	c2.innerText = e.rubric; 
 	c3.innerHTML = "v"; 
-	c5.innerHTML = "☒"; 
-	c4.classList.add("zoom"); 
-	c5.classList.add("zoom"); 
-	c4.addEventListener("click", () => edit(c4, i)); 
-	c5.addEventListener("click", () => del(e)); 
+	c3.classList.add("zoom"); 
+	//c3.addEventListener("click", () => edit(c3, i)); 
+	
 } 
 
 // Traverse and insert items to table 
@@ -39,71 +37,36 @@ data.map((e, i) => addItem(e, i));
 function sortItems(title) { 
 	remove(); 
 	switch (title) { 
-		case "name": 
-			sortName(); 
-			break; 
-		case "category": 
-			sortCat(); 
-			break; 
-		case "year": 
-			sortYear(); 
-			break; 
-		default: 
-			console.log("Default"); 
+		case "qstnName":
+		case "rubric":
+		case "modifiedAt":
+			data.sort((a, b) => { 
+				let fa = a[title].toLowerCase(), 
+				fb = b[title].toLowerCase(); 
+		
+				if (fa < fb) { 
+					return -1; 
+				} 
+				if (fa > fb) { 
+					return 1; 
+				} 
+				return 0; 
+			}); 
+			if (flag[title]) data.reverse(); 
+			flag[title] = !flag[title]; 
+			break;
+		
+			
 	} 
 	data.map((e, i) => addItem(e, i)); 
 } 
 
 // Clear the table before updation 
 function remove() { 
-	console.log("removed"); 
-	while (table.s.length > 1) table.delete(-1); 
+	while (table.rows.length > 1) table.delete(-1); 
 } 
 
-// Sort with names 
-function sortName() { 
-	data.sort((a, b) => { 
-		let fa = a.Name.toLowerCase(), 
-			fb = b.Name.toLowerCase(); 
-		console.log(fa, fb); 
 
-		if (fa < fb) { 
-			return -1; 
-		} 
-		if (fa > fb) { 
-			return 1; 
-		} 
-		return 0; 
-	}); 
-	if (flag.Name) data.reverse(); 
-	flag.Name = !flag.Name; 
-} 
-
-// Sort with categories 
-function sortCat() { 
-	data.sort((a, b) => { 
-		let fa = a.Cat.toLowerCase(), 
-			fb = b.Cat.toLowerCase(); 
-		console.log(fa, fb); 
-
-		if (fa < fb) { 
-			return -1; 
-		} 
-		if (fa > fb) { 
-			return 1; 
-		} 
-		return 0; 
-	}); 
-	if (flag.Cat) data.reverse(); 
-	flag.Cat = !flag.Cat; 
-} 
-
-// Sort with year 
-function sortYear() { 
-	data.sort((a, b) => a.Year - b.Year); 
-	if (flag.Year) data.reverse(); 
-	flag.Year = !flag.Year; 
-} 
 
 // To search and filter items 
 function searchItems() { 
@@ -112,45 +75,11 @@ function searchItems() {
 		.value.toLowerCase(); 
 	let filterItems = data.filter((e) => { 
 		return ( 
-			e.Name.toLowerCase().includes(input) || 
-			e.Cat.toLowerCase().includes(input) || 
-			e.Year.includes(input) 
+			e.qstnName.toLowerCase().includes(input)
 		); 
 	}); 
 
 	remove(); 
 	filterItems.map((e, i) => addItem(e, i)); 
 } 
-
-// Initiate edit form 
-function edit(c, i) { 
-	console.log(c.classList.value); 
-	if (c.classList.value === "zoom") { 
-		c.classList.add("open"); 
-		el = data[i]; 
-		switchEdit(); 
-
-		let nameInput = 
-			document.getElementById("nameInput"); 
-		let catInput = document.getElementById("catInput"); 
-		let yearInput = 
-			document.getElementById("yearInput"); 
-		nameInput.value = el.Name; 
-		catInput.value = el.Cat; 
-		yearInput.value = el.Year; 
-		index = i; 
-	} else { 
-		c.classList.value = "zoom"; 
-		switchAdd(); 
-
-		document.getElementById("nameInput").value = ""; 
-		document.getElementById("catInput").value = ""; 
-		document.getElementById("yearInput").value = ""; 
-		index = -1; 
-	} 
-} 
-
-
-
-
 
