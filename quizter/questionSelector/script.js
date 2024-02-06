84b56f6030cce21aa10836623c0f50f56c59c259
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2024-02-06 10:46:23 +0300

    87

--- quizter/questionSelector/script.js
+++ quizter/questionSelector/script.js
@@ -5,7 +5,7 @@ function myLog(msg) {
 	logBox.innerText = msg + '\n' + `${logBox.innerText || ''}`;
 }
 
-myLog('Версия 86');
+myLog('Версия 87');
 
 myLog('window.Telegram.WebApp.initDataUnsafe.start_param = ' + window.Telegram.WebApp.initDataUnsafe.start_param);
 myLog('window.location.search = ' + window.location.search);
@@ -341,6 +341,7 @@ function searchItems(searchStr) {
 			includes = includes || e[column].toLowerCase().includes(searchStr);			
 		}
 		e.filteredOut = !includes;
+		myLog(e.filteredOut);
 	}); 
 
 	removeTable(); 
