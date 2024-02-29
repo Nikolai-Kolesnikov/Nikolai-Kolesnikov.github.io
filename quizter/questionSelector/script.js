c3baf8c40b3875d95ec391ac157971c1399ca210
Nikolai-Kolesnikov <141136656+Nikolai-Kolesnikov@users.noreply.github.com>
2024-02-29 17:42:16 +0300

    137

--- quizter/questionSelector/script.js
+++ quizter/questionSelector/script.js
@@ -6,7 +6,7 @@ function myLog(msg) {
 	logBox.innerText = curDate.toLocaleString('ru-RU', { timeZone: 'Europe/Moscow' }) + ': ' + msg + '\n' + `${logBox.innerText || ''}`;
 }
 
-myLog('Версия 136');
+myLog('Версия 137');
 
 //myLog('window.Telegram.WebApp.initDataUnsafe.start_param = ' + window.Telegram.WebApp.initDataUnsafe.start_param);
 //myLog('window.location.search = ' + window.location.search);
@@ -217,7 +217,8 @@ for (const obj of settingsObj[startappJson.action]['aboveTable']) {
 					let queryName = inputChanged.getAttribute('data-setQuery-name');
 					let keyToSet = inputChanged.getAttribute('data-setQuery-keyToSet');
 					let valueToSet = inputChanged.value;
-					valueToSet = new Date(Date.parse(valueToSet) - 3*60*60*1000);
+					valueToSet = new Date(valueToSet);
+					valueToSet = new Date(valueToSet.getTime() - valueToSet.getTimezoneOffset()*60*1000 - 3*60*60*1000);
 					valueToSet = valueToSet.toISOString();
 					valueToSet = valueToSet.slice(0,-5);
 					let rData = {
